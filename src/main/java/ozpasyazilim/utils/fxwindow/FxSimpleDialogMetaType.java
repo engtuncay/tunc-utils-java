49da2c8e86a3290ca18675b66f296fcc923450a4
Tuncay <tunc270@gmail.com>
2024-01-18 11:51:41 +0300

    up

--- src/main/java/ozpasyazilim/utils/fxwindow/FxSimpleDialogMetaType.java
+++ src/main/java/ozpasyazilim/utils/fxwindow/FxSimpleDialogMetaType.java
@@ -2,12 +2,14 @@ package ozpasyazilim.utils.fxwindow;
 
 public enum FxSimpleDialogMetaType {
 	TextFieldDouble, TextFieldInteger, InfoTextFlowDialog, TextField, InfoLabelDialog,
+
 	/**
 	 * Class ın Cand Id field larından bir form oluşturur otomatik.
 	 */
 	FormAutoByCandIdFields,
 	DialogError, DialogInfo, TextFieldWithValidation,
 	ErrorDialogDetailed,
+
 	/**
 	 * FxForm üzerinden dialog oluşturur
 	 */
