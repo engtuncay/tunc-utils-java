48ff1998d1d67585f1b9ae52a5672ee0b6b2343d
TuncayHp2 <tunc270@gmail.com>
2026-05-18 16:35:48 +0300

    rfc ups - mikroAktarim- cekVerilen karsi kod banka ayarlandi

--- src/main/java/ozpasyazilim/utils/gui/components/DScrollPane.java
+++ src/main/java/ozpasyazilim/utils/gui/components/DScrollPane.java
@@ -4,11 +4,8 @@ import java.awt.Component;
 
 import javax.swing.JScrollPane;
 
-public class DScrollPane
-  extends JScrollPane
-{
-  public DScrollPane(Component view)
-  {
+public class DScrollPane extends JScrollPane {
+  public DScrollPane(Component view) {
     super(view);
   }
 }
