bd2708a54db47e0521f4544b33408528b35be805
TuncayHp2 <tunc270@gmail.com>
2026-07-24 10:37:40 +0300

    ups

--- src/main/java/ozpasyazilim/utils/gui/fxcomponents/FxMigHp.java
+++ src/main/java/ozpasyazilim/utils/gui/fxcomponents/FxMigHp.java
@@ -7,323 +7,324 @@ import ozpasyazilim.utils.table.OzColType;
 /**
  * layout constraints - 3'e ayrılır (lcg,lcc,lrc), fakat tek lcg tanımlanması yeterli olur.
  * <p>
- * new MigPane(String layoutConstraintsGeneral)
+ * new MigPane(String lcg) or new MigPane(String lcg, String lcc, String lrc)
  * <p>
- * veya new MigPane(String layoutConstraintsGeneral, String colConstraints, String rowConstraints) kullanılır.
+ * lcg:layout-constraints-general, lcc:layout-col-constr.,lrc:layout-row-constr. (contructor parametreleri)
  * <p>
- * lcg: layout constraints general ,lcc layout-col-contraints,lrc layout-row-contraints (contructor parametreleri)
+ * cc: cell-contraint ise migpane instance'na node eklerken (add ile) verdiğimiz kısıtlar.
  * <p>
- * cc : cell contraints ise migpane instance'na node eklerken verdiğimiz kısıtlar.
- * <p>
- * migpane.addNode(Node node, String cellConstraints) metodunda kullanılır.
+ * migpane.addNode(Node node, String cc) metodunda kullanılır.
  * <p>
  * bui metodu ile instance oluşturulup kullanılır (wrapping system)
  */
 public class FxMigHp {
 
-    /**
-     * Layout Constraits General (constructor based)
-     */
-    private String layConstGen; // lcg
+  //public static String lcStandardInset0Gap55 = "insets 0,gap 5 5"; // gap x y : x yatatya boşluk : y dikeyde boşluk
+  //public static String lcDebug = ",debug";
 
-    /**
-     * Layout Column Constraints (constructor based)
-     */
-    private String layColConst; // lcc
+  /**
+   * Layout Constraits General (constructor based)
+   */
+  private String layConstGen; // lcg
 
-    /**
-     * Layout Row Constraints (constructor based)
-     */
-    private String layRowConst; // lrc
+  /**
+   * Layout Column Constraints (constructor based)
+   */
+  private String layColConst; // lcc
 
-    private String cellConst; //cc
+  /**
+   * Layout Row Constraints (constructor based)
+   */
+  private String layRowConst; // lrc
 
-    public static Boolean debugMode = false;
+  private String cellConst; //cc
 
-    // Statik degil get ile yapalım.
-    // gap x y : x yatatya boşluk : y dikeyde boşluk
-    @Deprecated
-    public static String lcgStandard1InsetZeroGap50 = "insets 0,gap 5 0";
-    @Deprecated
-    public static String lcgStandard1InsetZeroGap00 = "insets 0,gap 0 0";
+  public static Boolean debugMode = false;
 
-    public static FxMigHp bui() {
-        return new FxMigHp();
-    }
+  // Statik degil get ile yapalım.
+  // gap x y : x yatatya boşluk : y dikeyde boşluk
+  @Deprecated
+  public static String lcgStandard1InsetZeroGap50 = "insets 0,gap 5 0";
+  @Deprecated
+  public static String lcgStandard1InsetZeroGap00 = "insets 0,gap 0 0";
 
-    public static FxMigHp bcc(String cc) {
-        FxMigHp fxMigHp = new FxMigHp();
-        fxMigHp.setCellConst(cc);
-        return fxMigHp;
-    }
+  public static FxMigHp bui() {
+    return new FxMigHp();
+  }
 
-    public static FxMigHp blc(String lc) {
-        FxMigHp fxMigHp = new FxMigHp();
-        fxMigHp.setLayConstGen(lc);
-        return fxMigHp;
-    }
+  public static FxMigHp bcc(String cc) {
+    FxMigHp fxMigHp = new FxMigHp();
+    fxMigHp.setCellConst(cc);
+    return fxMigHp;
+  }
 
-    public static FxMigHp blc(String lgc, String rc, String lcc) {
-        FxMigHp fxMigHp = new FxMigHp();
-        fxMigHp.setLayConstGen(lgc);
-        fxMigHp.setLayRowConst(rc);
-        fxMigHp.setLayColConst(lcc);
-        return fxMigHp;
-    }
+  public static FxMigHp blc(String lc) {
+    FxMigHp fxMigHp = new FxMigHp();
+    fxMigHp.setLayConstGen(lc);
+    return fxMigHp;
+  }
 
-    public FxMigHp ccWidth(String txWidth) {
-        addCommaToCc();
-        cellConst += "w " + txWidth;
-        return this;
-    }
+  public static FxMigHp blc(String lgc, String rc, String lcc) {
+    FxMigHp fxMigHp = new FxMigHp();
+    fxMigHp.setLayConstGen(lgc);
+    fxMigHp.setLayRowConst(rc);
+    fxMigHp.setLayColConst(lcc);
+    return fxMigHp;
+  }
 
-    public FxMigHp lcgInsetAndGap(Integer inset, Integer gapx, Integer gapy) {
-        appendToLcg(String.format("insets %s,gap %s %s", inset, gapx, gapy));
-        return this;
-    }
-    // panelLayout = new MigLayout("insets 0 0 0 0");
-    // setLayout(new MigLayout("gap rel 0", "grow"));
-    //setBorder(BorderFactory.createTitledBorder("Sterowanie:"));
+  public FxMigHp ccWidth(String txWidth) {
+    addCommaToCc();
+    cellConst += "w " + txWidth;
+    return this;
+  }
 
-    /**
-     * @return
-     */
-    public FxMigHp lcgNoGrid() {
-        appendToLcg("nogrid");
-        return this;
-    }
+  public FxMigHp lcgInsetAndGap(Integer inset, Integer gapx, Integer gapy) {
+    appendToLcg(String.format("insets %s,gap %s %s", inset, gapx, gapy));
+    return this;
+  }
+  // panelLayout = new MigLayout("insets 0 0 0 0");
+  // setLayout(new MigLayout("gap rel 0", "grow"));
+  //setBorder(BorderFactory.createTitledBorder("Sterowanie:"));
 
-    private void appendToLcg(String append) {
-        addCommaLayConstGenPrev();
-        this.layConstGen += append;
-    }
+  /**
+   * @return
+   */
+  public FxMigHp lcgNoGrid() {
+    appendToLcg("nogrid");
+    return this;
+  }
 
-    public FxMigHp lcgAlignx(Integer percent) {
-        appendToLcg("ax " + percent + "%");
-        return this;
-    }
+  private void appendToLcg(String append) {
+    addCommaLayConstGenPrev();
+    this.layConstGen += append;
+  }
 
-    public FxMigHp lcgInset0Gap55() {
-        lcgInsetAndGap(0, 5, 5);
-        return this;
-    }
+  public FxMigHp lcgAlignx(Integer percent) {
+    appendToLcg("ax " + percent + "%");
+    return this;
+  }
 
-    public FxMigHp lcgInset0Gap33() {
-        lcgInsetAndGap(0, 3, 3);
-        return this;
-    }
+  public FxMigHp lcgInset0Gap55() {
+    lcgInsetAndGap(0, 5, 5);
+    return this;
+  }
+
+  public FxMigHp lcgInset0Gap33() {
+    lcgInsetAndGap(0, 3, 3);
+    return this;
+  }
 
 
-    public FxMigHp lcgInset0Gap50() {
-        lcgInsetAndGap(0, 5, 0);
-        return this;
-    }
+  public FxMigHp lcgInset0Gap50() {
+    lcgInsetAndGap(0, 5, 0);
+    return this;
+  }
 
-    public FxMigHp lcgInset0Gap00() {
-        lcgInsetAndGap(0, 0, 0);
-        return this;
-    }
+  public FxMigHp lcgInset0Gap00() {
+    lcgInsetAndGap(0, 0, 0);
+    return this;
+  }
 
-    public FxMigHp lcgInset0Gap03() {
-        lcgInsetAndGap(0, 0, 3);
-        return this;
-    }
+  public FxMigHp lcgInset0Gap03() {
+    lcgInsetAndGap(0, 0, 3);
+    return this;
+  }
 
-    public FxMigHp lcgInset0Gap05() {
-        lcgInsetAndGap(0, 0, 5);
-        return this;
-    }
+  public FxMigHp lcgInset0Gap05() {
+    lcgInsetAndGap(0, 0, 5);
+    return this;
+  }
 
 //	public FxMigHp lcgInset0Gap010() {
 //		lcgInsetAndGap(0, 0,10 );
 //		return this;
 //	}
 
-    public FxMigHp lcgInset3Gap00() {
-        lcgInsetAndGap(3, 0, 0);
-        return this;
-    }
+  public FxMigHp lcgInset3Gap00() {
+    lcgInsetAndGap(3, 0, 0);
+    return this;
+  }
 
-    public FxMigHp lcgInset3Gap33() {
-        lcgInsetAndGap(3, 3, 3);
-        return this;
-    }
+  public FxMigHp lcgInset3Gap33() {
+    lcgInsetAndGap(3, 3, 3);
+    return this;
+  }
 
-    private void addCommaLayConstGenPrev() {
-        if (FiString.isEmptyTrim(getLcgRawInit())) return;
-        this.layConstGen += ",";
-    }
+  private void addCommaLayConstGenPrev() {
+    if (FiString.isEmptyTrim(getLcgRawInit())) return;
+    this.layConstGen += ",";
+  }
 
-    private void addCommaToCc() {
-        if (FiString.isEmptyTrim(getCcInit())) return;
-        this.cellConst += ",";
-    }
+  private void addCommaToCc() {
+    if (FiString.isEmptyTrim(getCcInit())) return;
+    this.cellConst += ",";
+  }
 
-    public String getLcgPrep2() {
+  public String getLcgPrep2() {
 //		if (layConstGen == null) {
 //			layConstGen = "";
 //		}
 //		return layConstGen;
-        return getLcg();
+    return getLcg();
+  }
+
+  public String getLcgRawInit() {
+    if (layConstGen == null) {
+      layConstGen = "";
     }
+    return layConstGen;
+  }
 
-    public String getLcgRawInit() {
-        if (layConstGen == null) {
-            layConstGen = "";
-        }
-        return layConstGen;
+  public String getLayRowConst() {
+    if (layRowConst == null) {
+      layRowConst = "";
     }
+    return layRowConst;
+  }
 
-    public String getLayRowConst() {
-        if (layRowConst == null) {
-            layRowConst = "";
-        }
-        return layRowConst;
+  public String getLayColConst() {
+    if (layColConst == null) {
+      layColConst = "";
     }
+    return layColConst;
+  }
 
-    public String getLayColConst() {
-        if (layColConst == null) {
-            layColConst = "";
-        }
-        return layColConst;
-    }
+  /**
+   * Layout Constraints
+   * <p>
+   * MigPane constructor na yazılır  (Lc,Lrc,Lcc)
+   *
+   * @return
+   */
+  public String getLcg() {
+    if (layConstGen == null) layConstGen = "";
 
-    /**
-     * Layout Constraints
-     * <p>
-     * MigPane constructor na yazılır  (Lc,Lrc,Lcc)
-     *
-     * @return
-     */
-    public String getLcg() {
-        if (layConstGen == null) layConstGen = "";
-
-        if (debugMode) {
-            if (!layConstGen.contains("debug")) {
-                appendToLcg("debug");
-            }
-        }
-        return layConstGen;
-    }
-
-    /**
-     * Layout Row Constraints
-     *
-     * @return
-     */
-    public String genLrc() {
-        return layRowConst;
-    }
-
-    /**
-     * Layout Column Constraint
-     *
-     * @return
-     */
-    public String genLcc() {
-        return layColConst;
-    }
-
-    /**
-     * Component constraint
-     *
-     * @return
-     */
-    public String genCc() {
-        return cellConst;
-    }
-
-    public String getCcInit() {
-        if (cellConst == null) {
-            cellConst = "";
-        }
-        return cellConst;
-    }
-
-    public String getCc() {
-        return cellConst;
-    }
-
-    public FxMigHp lcgDebug() {
+    if (debugMode) {
+      if (!layConstGen.contains("debug")) {
         appendToLcg("debug");
-        return this;
+      }
+    }
+    return layConstGen;
+  }
+
+  /**
+   * Layout Row Constraints
+   *
+   * @return
+   */
+  public String genLrc() {
+    return layRowConst;
+  }
+
+  /**
+   * Layout Column Constraint
+   *
+   * @return
+   */
+  public String genLcc() {
+    return layColConst;
+  }
+
+  /**
+   * Component constraint
+   *
+   * @return
+   */
+  public String genCc() {
+    return cellConst;
+  }
+
+  public String getCcInit() {
+    if (cellConst == null) {
+      cellConst = "";
+    }
+    return cellConst;
+  }
+
+  public String getCc() {
+    return cellConst;
+  }
+
+  public FxMigHp lcgDebug() {
+    appendToLcg("debug");
+    return this;
+  }
+
+  public void setLayConstGen(String layConstGen) {
+    this.layConstGen = layConstGen;
+  }
+
+  public void setLayRowConst(String layRowConst) {
+    this.layRowConst = layRowConst;
+  }
+
+  public void setLayColConst(String layColConst) {
+    this.layColConst = layColConst;
+  }
+
+  public void setCellConst(String cellConst) {
+    this.cellConst = cellConst;
+  }
+
+  public FxMigHp addCcCompMaxWidthSizeByColTypeForFxForm(FiCol fiCol) {
+
+    if (fiCol.getColType() == OzColType.Date || fiCol.getColEditorClass().equals(FxDatePicker.class.getName())) {
+      appendCc("wmax 150");
     }
 
-    public void setLayConstGen(String layConstGen) {
-        this.layConstGen = layConstGen;
+    if (fiCol.getColType() == OzColType.Double) {
+      appendCc("wmax 200");
     }
 
-    public void setLayRowConst(String layRowConst) {
-        this.layRowConst = layRowConst;
+    if (fiCol.getColType() == OzColType.Integer) {
+      appendCc("wmax 150");
     }
 
-    public void setLayColConst(String layColConst) {
-        this.layColConst = layColConst;
-    }
+    return this;
+  }
 
-    public void setCellConst(String cellConst) {
-        this.cellConst = cellConst;
-    }
+  public FxMigHp ccWmin(String txWminValue) {
+    appendCc("wmin " + txWminValue);
+    return this;
+  }
 
-    public FxMigHp addCcCompMaxWidthSizeByColTypeForFxForm(FiCol fiCol) {
+  private void appendCc(String value) {
+    addCommaToCc();
+    setCellConst(getCcInit() + value);
+  }
 
-        if (fiCol.getColType() == OzColType.Date || fiCol.getColEditorClass().equals(FxDatePicker.class.getName())) {
-            appendCc("wmax 150");
-        }
+  public FxMigHp ccAlignYTop() {
+    appendCc("aligny top");
+    return this;
+  }
 
-        if (fiCol.getColType() == OzColType.Double) {
-            appendCc("wmax 200");
-        }
+  public FxMigHp lcgInset5Gap00() {
+    lcgInsetAndGap(5, 0, 0);
+    return this;
+  }
 
-        if (fiCol.getColType() == OzColType.Integer) {
-            appendCc("wmax 150");
-        }
+  public FxMigHp lcgInset5Gap05() {
+    lcgInsetAndGap(5, 0, 5);
+    return this;
+  }
 
-        return this;
-    }
+  public FxMigHp lcgInset5Gap55() {
+    lcgInsetAndGap(5, 5, 5);
+    return this;
+  }
 
-    public FxMigHp ccWmin(String txWminValue) {
-        appendCc("wmin " + txWminValue);
-        return this;
-    }
+  public FxMigHp ccPushxGrowxSpan() {
+    appendCc("pushx,growx,span");
+    return this;
+  }
 
-    private void appendCc(String value) {
-        addCommaToCc();
-        setCellConst(getCcInit() + value);
-    }
+  public FxMigHp ccGapAfter(String txGapxPx) {
+    appendCc("gapafter " + txGapxPx);
+    return this;
+  }
 
-    public FxMigHp ccAlignYTop() {
-        appendCc("aligny top");
-        return this;
-    }
-
-    public FxMigHp lcgInset5Gap00() {
-        lcgInsetAndGap(5, 0, 0);
-        return this;
-    }
-
-    public FxMigHp lcgInset5Gap05() {
-        lcgInsetAndGap(5, 0, 5);
-        return this;
-    }
-
-    public FxMigHp lcgInset5Gap55() {
-        lcgInsetAndGap(5, 5, 5);
-        return this;
-    }
-
-    public FxMigHp ccPushxGrowxSpan() {
-        appendCc("pushx,growx,span");
-        return this;
-    }
-
-    public FxMigHp ccGapAfter(String txGapxPx) {
-        appendCc("gapafter "+ txGapxPx);
-        return this;
-    }
-
-    public FxMigHp ccGapBefore(String txGapxPx) {
-        appendCc("gapbefore "+ txGapxPx);
-        return this;
-    }
+  public FxMigHp ccGapBefore(String txGapxPx) {
+    appendCc("gapbefore " + txGapxPx);
+    return this;
+  }
 }
--- src/main/java/ozpasyazilim/utils/gui/fxcomponents/FxMigPane.java
+++ src/main/java/ozpasyazilim/utils/gui/fxcomponents/FxMigPane.java
@@ -9,163 +9,159 @@ import org.tbee.javafx.scene.layout.MigPane;
 import ozpasyazilim.utils.core.FiString;
 
 /**
- * Constraint Shortcuts
+ * Constraint Abbr.
  * <p>
- * cc component constraint
+ * cc component constraint, cell constraint  (migpane'e add yapılırken verilen constraint)
  * <p>
- * lcg layout constraint general
+ * lcg layout-constraint-general,lrc:layout-row-constr., lcc:layout-col-constr.
  * <p>
- * lrc layout row constraint
- * <p>
- * lcc layout col constraint
+ * Constraint yardımcı {@link FxMigHp} class ını kulllan
  */
 public class FxMigPane extends MigPane {
 
-	//public static String lcStandardInset0Gap55 = "insets 0,gap 5 5"; // gap x y : x yatatya boşluk : y dikeyde boşluk
-	//public static String lcDebug = ",debug";
+  public FxMigPane() {
+    super();
+  }
 
-	public FxMigPane() {
-		super();
-	}
+  public FxMigPane(LC layoutConstraints) {
+    super(layoutConstraints);
+  }
 
-	public FxMigPane(LC layoutConstraints) {
-		super(layoutConstraints);
-	}
+  public FxMigPane(LC layoutConstraints, AC colConstraints) {
+    super(layoutConstraints, colConstraints);
+  }
 
-	public FxMigPane(LC layoutConstraints, AC colConstraints) {
-		super(layoutConstraints, colConstraints);
-	}
+  public FxMigPane(LC layoutConstraints, AC colConstraints, AC rowConstraints) {
+    super(layoutConstraints, colConstraints, rowConstraints);
+  }
 
-	public FxMigPane(LC layoutConstraints, AC colConstraints, AC rowConstraints) {
-		super(layoutConstraints, colConstraints, rowConstraints);
-	}
+  public FxMigPane(String layoutConstraints) {
+    super(layoutConstraints);
+  }
 
-	public FxMigPane(String layoutConstraints) {
-		super(layoutConstraints);
-	}
+  public FxMigPane(String layoutConstraints, String colConstraints) {
+    super(layoutConstraints, colConstraints);
+  }
 
-	public FxMigPane(String layoutConstraints, String colConstraints) {
-		super(layoutConstraints, colConstraints);
-	}
+  public FxMigPane(String layoutConstraints, String colConstraints, String rowConstraints) {
+    super(layoutConstraints, colConstraints, rowConstraints);
+  }
 
-	public FxMigPane(String layoutConstraints, String colConstraints, String rowConstraints) {
-		super(layoutConstraints, colConstraints, rowConstraints);
-	}
+  public FxMigPane(FxMigHp fxMigHp) {
+    super(fxMigHp.getLcgPrep2(), fxMigHp.getLayColConst(), fxMigHp.getLayRowConst());
+  }
 
-	public FxMigPane(FxMigHp fxMigHp) {
-		super(fxMigHp.getLcgPrep2(), fxMigHp.getLayColConst(), fxMigHp.getLayRowConst());
-	}
+  public static String ccHeight(int height) {
+    return ",h " + height;
+  }
 
-	public static String ccHeight(int height) {
-		return ",h " + height;
-	}
+  public static String ccHeightPerc(int heightPercentage) {
+    return ",h " + heightPercentage + "%";
+  }
 
-	public static String ccHeightPerc(int heightPercentage) {
-		return ",h " + heightPercentage + "%";
-	}
+  public static String ccWidth(int width) {
+    return ",w " + width;
+  }
 
-	public static String ccWidth(int width) {
-		return ",w " + width;
-	}
+  public static String ccPrefWidth(int i) {
+    return ",w :" + i + ":" + i;
+  }
 
-	public static String ccPrefWidth(int i) {
-		return ",w :" + i + ":" + i;
-	}
+  public static FxMigPane buiStandard() {
+    return new FxMigPane(FxMigHp.lcgStandard1InsetZeroGap00);
+  }
 
-	public static FxMigPane buiStandard() {
-		return new FxMigPane(FxMigHp.lcgStandard1InsetZeroGap00);
-	}
+  public void wrapFi() {
+    add(new FxLabel(""), "span,wrap");
+  }
 
-	public void wrapFi() {
-		add(new FxLabel(""), "span,wrap");
-	}
+  public void addGrowXSpan(Node node) {
+    add(node, "growx,pushx,span");
+  }
 
-	public void addGrowXSpan(Node node) {
-		add(node, "growx,pushx,span");
-	}
+  public void addSpanWrap(Node node) {
+    add(node, "span,wrap");
+  }
 
-	public void addSpanWrap(Node node) {
-		add(node, "span,wrap");
-	}
+  public void addGrowXSpan(Node node, String extra) {
+    add(node, appendExtra("growx,pushx,span", extra));
+  }
 
-	public void addGrowXSpan(Node node, String extra) {
-		add(node, appendExtra("growx,pushx,span", extra));
-	}
+  /**
+   * {@link FxMigHp} cc (cell constraint) yardımcı sınıfı kullan
+   *
+   * @param node
+   * @param txCustomConstraints
+   */
+  public void addCustom(Node node, String txCustomConstraints) {
+    add(node, txCustomConstraints);
+  }
 
-	/**
-	 * {@link FxMigHp} cc (cell constraint) yardımcı sınıfı kullan
-	 *
-	 * @param node
-	 * @param txCustomConstraints
-	 */
-	public void addCustom(Node node, String txCustomConstraints) {
-		add(node, txCustomConstraints);
-	}
+  public void addGrowXPushXSpan(Node node, String extra) {
+    add(node, appendExtra("growx,pushx,span", extra));
+  }
 
-	public void addGrowXPushXSpan(Node node, String extra) {
-		add(node, appendExtra("growx,pushx,span", extra));
-	}
+  private String appendExtra(String constraint, String extra) {
 
-	private String appendExtra(String constraint, String extra) {
+    if (FiString.isEmptyTrim(extra)) {
+      return constraint;
+    }
+    // extra is full
+    String seperator = FiString.getCommaIfNotEmpty(constraint);
+    return FiString.orEmpty(constraint) + seperator + extra;
+  }
 
-		if (FiString.isEmptyTrim(extra)) {
-			return constraint;
-		}
-		// extra is full
-		String seperator = FiString.getCommaIfNotEmpty(constraint);
-		return FiString.orEmpty(constraint) + seperator + extra;
-	}
+  public void addGrowPushSpan(Node node) {
+    add(node, "grow,push,span");
+  }
 
-	public void addGrowPushSpan(Node node) {
-		add(node, "grow,push,span");
-	}
+  public void addWrap(Node node) {
+    add(node, "wrap");
+  }
 
-	public void addWrap(Node node) {
-		add(node, "wrap");
-	}
+  public void addGrowPushSpan(Node node, String extra) {
+    add(node, appendExtra("grow,push,span", extra));
+  }
 
-	public void addGrowPushSpan(Node node, String extra) {
-		add(node, appendExtra("grow,push,span", extra));
-	}
+  public void addGrowSpan(Node node, String extra) {
+    add(node, appendExtra("grow,span", extra));
+  }
 
-	public void addGrowSpan(Node node, String extra) {
-		add(node, appendExtra("grow,span", extra));
-	}
+  public void addSpan(Node node) {
+    add(node, "span");
+  }
 
-	public void addSpan(Node node) {
-		add(node, "span");
-	}
+  public void addSpan(Node node, String extra) {
+    add(node, appendExtra("span", extra));
+  }
 
-	public void addSpan(Node node,String extra) {
-		add(node, appendExtra("span",extra));
-	}
+  public void addGrowXPushXSpan(Node node) {
+    addGrowXPushXSpan(node, "growx,pushx,span");
+  }
 
-	public void addGrowXPushXSpan(Node node) {
-		addGrowXPushXSpan(node, "growx,pushx,span");
-	}
+  /**
+   * Yatay çizgi ekler
+   *
+   * @return seperator node
+   */
+  public Separator addSeparatorLineHor() {
+    Separator separator = new Separator(Orientation.HORIZONTAL);
+    addGrowXSpan(separator);
+    return separator;
+  }
 
-	/**
-	 * Yatay çizgi ekler
-	 *
-	 * @return seperator node
-	 */
-	public Separator addSeparatorLineHor() {
-		Separator separator = new Separator(Orientation.HORIZONTAL);
-		addGrowXSpan(separator);
-		return separator;
-	}
+  public FxMigPane addFi(Node node) {
+    add(node);
+    return this;
+  }
 
-	public FxMigPane addFi(Node node) {
-		add(node);
-		return this;
-	}
-
-	/**
-	 * root mig'e eklenirse align right yapıyor, mig'içinde
-	 * @param node
-	 */
-	public void addAlignxRight(Node node) {
-		add(node, "alignx right,span"); //alignx right,span
-	}
+  /**
+   * root mig'e eklenirse align right yapıyor, mig'içinde
+   *
+   * @param node
+   */
+  public void addAlignxRight(Node node) {
+    add(node, "alignx right,span"); //alignx right,span
+  }
 
 }
