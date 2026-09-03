80b492563f1b1113cd8c5ce9bd9e1ff024c0f43d
TuncayHp2 <tunc270@gmail.com>
2026-09-03 16:34:12 +0300

    ups and rfc

--- src/main/java/ozpasyazilim/utils/gui/fxcomponents/FiFxWindow.java
+++ src/main/java/ozpasyazilim/utils/gui/fxcomponents/FiFxWindow.java
@@ -14,103 +14,108 @@ import ozpasyazilim.utils.mvc.IFiModCont;
  */
 public class FiFxWindow {
 
-    public static void creNodeWindow(Node nodeRelative, IFiModCont ifxSimpleModCont) {
-        creNodeWindow(nodeRelative, ifxSimpleModCont, null, null, null);
+  public static void creNodeWindow(Node nodeRelative, IFiModCont ifxSimpleModCont) {
+    creNodeWindow(nodeRelative, ifxSimpleModCont, null, null, null);
+  }
+
+  public static void creNodeWindow(IFiModCont iFiModCont, DialogConf dialogConf) {
+
+    if (dialogConf == null) dialogConf = new DialogConf();
+
+    Pane rootPane = iFiModCont.getModView().getRootPane();
+
+    if (rootPane == null) {
+      FxDialogShow.showPopError("rootPane tanımlanmamış. sistem yöneticinize danışın.");
+      return;
     }
 
-    public static void creNodeWindow(IFiModCont iFiModCont, DialogConf dialogConf) {
+    FxWindowHelper.setMaxHeightAndWidthForWindows(rootPane);
 
-        if (dialogConf == null) dialogConf = new DialogConf();
-
-        Pane rootPane = iFiModCont.getModView().getRootPane();
-
-        FxWindowHelper.setMaxHeightAndWidthForWindows(rootPane);
-
-        if (!FiString.isEmpty(dialogConf.getCssFileName())) {
-            rootPane.getStylesheets().add(dialogConf.getCssFileName());
-        }
-
-        Stage stage = iFiModCont.getFxStageInit();
-
-        if (stage == null) {
-            stage = new FxStage();
-            iFiModCont.setFxStage(stage);
-        }
-
-        if (!FiString.isEmpty(dialogConf.getTitle())) {
-            stage.setTitle(dialogConf.getTitle());
-        }
-
-        FxScene scene = null;
-
-        if (dialogConf.getWidth() != null && dialogConf.getHeight() != null) {
-            scene = new FxScene(rootPane, dialogConf.getWidth(), dialogConf.getHeight());  //,width,height
-        } else {
-            scene = new FxScene(rootPane);  //,width,height
-        }
-
-        if (dialogConf.getNodeRelative() != null) {
-            Bounds bounds = dialogConf.getNodeRelative().localToScreen(dialogConf.getNodeRelative().getBoundsInLocal());
-            stage.setX(bounds.getMaxX());
-            stage.setY(bounds.getMinY());
-        }
-
-        stage.setScene(scene);
-        //stage.sizeToScene();
-
-        if (dialogConf.getWidth() != null) {
-            //Loghelper.debug(getClass(), "width ayarlanıyor" + dialogContext.getWidth());
-            //stage.setWidth(dialogConfig.getWidth());
-            rootPane.setPrefWidth(dialogConf.getWidth());
-        }
-
-        if (dialogConf.getWidth() == null && dialogConf.getHeight() == null) {
-            stage.sizeToScene();
-        }
-
-        if (FiBool.isTrue(dialogConf.getBoNonModal())) {
-            stage.initModality(Modality.WINDOW_MODAL);
-        } else {
-            stage.initModality(Modality.APPLICATION_MODAL);
-        }
-        stage.showAndWait();
-
+    if (!FiString.isEmpty(dialogConf.getCssFileName())) {
+      rootPane.getStylesheets().add(dialogConf.getCssFileName());
     }
 
-    public static void creNodeWindow(Node nodeRelative, IFiModCont iFiModCont, Integer width, Integer height, Boolean isNonModal) {
-        //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont Giriş");
+    Stage stage = iFiModCont.getFxStageInit();
 
-        Stage stage = iFiModCont.getFxStageInit();
-        //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont");
-
-        if (stage == null) {
-            //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont-A1-Yeni stage-");
-            stage = new FxStage();
-            iFiModCont.setFxStage(stage);
-        }
-
-        FxScene scene = null;
-
-        if (width != null && height != null) {
-            scene = new FxScene(iFiModCont.getModView().getRootPane(), width, height);  //,width,height
-        } else {
-            scene = new FxScene(iFiModCont.getModView().getRootPane());  //,width,height
-        }
-
-        if (nodeRelative != null) {
-            Bounds bounds = nodeRelative.localToScreen(nodeRelative.getBoundsInLocal());
-            stage.setX(bounds.getMaxX());
-            stage.setY(bounds.getMinY());
-        }
-
-        stage.setScene(scene);
-        stage.sizeToScene();
-        if (FiBool.isTrue(isNonModal)) {
-            stage.initModality(Modality.APPLICATION_MODAL);
-        } else {
-            stage.initModality(Modality.WINDOW_MODAL);
-        }
-
-        stage.showAndWait();
+    if (stage == null) {
+      stage = new FxStage();
+      iFiModCont.setFxStage(stage);
     }
+
+    if (!FiString.isEmpty(dialogConf.getTitle())) {
+      stage.setTitle(dialogConf.getTitle());
+    }
+
+    FxScene scene = null;
+
+    if (dialogConf.getWidth() != null && dialogConf.getHeight() != null) {
+      scene = new FxScene(rootPane, dialogConf.getWidth(), dialogConf.getHeight());  //,width,height
+    } else {
+      scene = new FxScene(rootPane);  //,width,height
+    }
+
+    if (dialogConf.getNodeRelative() != null) {
+      Bounds bounds = dialogConf.getNodeRelative().localToScreen(dialogConf.getNodeRelative().getBoundsInLocal());
+      stage.setX(bounds.getMaxX());
+      stage.setY(bounds.getMinY());
+    }
+
+    stage.setScene(scene);
+    //stage.sizeToScene();
+
+    if (dialogConf.getWidth() != null) {
+      //Loghelper.debug(getClass(), "width ayarlanıyor" + dialogContext.getWidth());
+      //stage.setWidth(dialogConfig.getWidth());
+      rootPane.setPrefWidth(dialogConf.getWidth());
+    }
+
+    if (dialogConf.getWidth() == null && dialogConf.getHeight() == null) {
+      stage.sizeToScene();
+    }
+
+    if (FiBool.isTrue(dialogConf.getBoNonModal())) {
+      stage.initModality(Modality.WINDOW_MODAL);
+    } else {
+      stage.initModality(Modality.APPLICATION_MODAL);
+    }
+    stage.showAndWait();
+
+  }
+
+  public static void creNodeWindow(Node nodeRelative, IFiModCont iFiModCont, Integer width, Integer height, Boolean isNonModal) {
+    //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont Giriş");
+
+    Stage stage = iFiModCont.getFxStageInit();
+    //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont");
+
+    if (stage == null) {
+      //Loghelper.get(getClass()).debug("nodeModalByIFxSimpleCont-A1-Yeni stage-");
+      stage = new FxStage();
+      iFiModCont.setFxStage(stage);
+    }
+
+    FxScene scene = null;
+
+    if (width != null && height != null) {
+      scene = new FxScene(iFiModCont.getModView().getRootPane(), width, height);  //,width,height
+    } else {
+      scene = new FxScene(iFiModCont.getModView().getRootPane());  //,width,height
+    }
+
+    if (nodeRelative != null) {
+      Bounds bounds = nodeRelative.localToScreen(nodeRelative.getBoundsInLocal());
+      stage.setX(bounds.getMaxX());
+      stage.setY(bounds.getMinY());
+    }
+
+    stage.setScene(scene);
+    stage.sizeToScene();
+    if (FiBool.isTrue(isNonModal)) {
+      stage.initModality(Modality.APPLICATION_MODAL);
+    } else {
+      stage.initModality(Modality.WINDOW_MODAL);
+    }
+
+    stage.showAndWait();
+  }
 }
